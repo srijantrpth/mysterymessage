61b05c1c4b06e18d93d8c801e0f6db7c8cd338ec
Srijan Tripathi <srijantrpth@gmail.com>
2025-07-12 21:42:38 +0530

    Add mongoose models and validation schemas for user messages
    - Introduced User and Message models using mongoose.
    - Added validation schemas for user sign-up, sign-in, message acceptance, and verification using zod.
    - Updated package.json and package-lock.json to include mongoose and zod dependencies.

---
+++ src/models/User.models.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    requried: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+export interface User extends Document {
+  username: string;
+  password: string;
+  email: string;
+  verifyCodeExpiry: Date;
+  verifyCode: string;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+  isVerified: boolean;
+}
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required! "],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required! "],
+    trim: true,
+    unique: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid Email Address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required! "],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is required! "],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify Code Expiry is Required! "],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
