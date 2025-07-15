18e087b5800e16eb3673708be9642a43b7d607b0
Srijan Tripathi <srijantrpth@gmail.com>
2025-07-15 21:09:52 +0530

    feat: implement user registration with email verification

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,4 +1,28 @@
-import {resend} from "@/lib/resend"
-import VerificationEmail from "../../emails/VerificationEmail"
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
 
-
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: email,
+    subject: 'Verification Code for Mystery Message',
+    react: VerificationEmail({username: username, otp: verifyCode}),
+  });
+    return {
+      success: true,
+      message: "Verification email sent successfully!  ",
+    };
+  } catch (emailError) {
+    console.error(`Error sending verification Email! `, emailError);
+    return {
+      success: false,
+      message: "Failed to send verification Email! ",
+    };
+  }
+}
