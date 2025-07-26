11c3e9debd64c05a06cc26ac39e42f58b410b6b9
Srijan Tripathi <srijantrpth@gmail.com>
2025-07-26 14:14:08 +0530

    feat: enhance username uniqueness check and add user verification functionality

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -16,15 +16,41 @@ export async function GET(request: Request) {
     };
     const result = UsernameQuerySchema.safeParse(queryParam);
     console.log(result);
-    if(!result.success){
-        const usernameErrors = result.error.format().username?._errors || [];
-        return Response.json({
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json(
+        {
           success: false,
-          message: ""
-        })
-        
+          message:
+            usernameErrors.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid Query Parameters! ",
+        },
+        { status: 400 }
+      );
     }
-    
+    const { username } = result.data;
+    const existingVerifiedUser = await UserModel.findOne({username, isVerified: true})
+    if(!existingVerifiedUser){
+        return Response.json(
+         {
+          success: true,
+          message:
+        "Username is available! "
+        },
+        { status: 201 }
+      );
+    }
+    return Response.json(
+         {
+          success: false,
+          message:
+        "Username is already taken! "
+        },
+        { status: 400 }
+     
+      );
+
   } catch (error) {
     console.error("Error checking username", error);
     return Response.json(
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -4,8 +4,7 @@ export const usernameValidation = z
     .string()
     .min(2, "Username must be at least 2 characters! ")
     .max(20, "Username must be no more than 20 characters! ")
-    .regex(/[^a-zA-Z0-9 ]/,"Username must not contain special character")
-
+  .regex(/^[a-zA-Z0-9]+$/, "Username must only contain letters and numbers"); //  Fixed regex
 export const signUpSchema = z.object({
   username: usernameValidation,
   email: z.string().email({message: "Invalid Email Address! "}),
