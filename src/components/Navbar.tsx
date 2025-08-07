f2a3e85a9bf4b24921e8120ccd7afc3cc65a9ecf
Srijan Tripathi <srijantrpth@gmail.com>
2025-08-07 14:08:12 +0530

    fix: update Button import path in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { User } from "next-auth";
-import { Button } from "./button";
+import { Button } from "./ui/button";
 const Navbar = () => {
   const { data: session } = useSession();
   const user: User = session?.user as User;
